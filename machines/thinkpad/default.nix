25eac0e68736dee4906efad833187e8a6c124a83
dettlaff <dettlaff@riseup.net>
2024-03-23 21:07:33 +0000

    Update default.nix

--- machines/thinkpad/default.nix
+++ machines/thinkpad/default.nix
@@ -6,7 +6,7 @@
 
     ../../modules
     ../../hardware/thinkpad.nix
-    ../../machines/thinkpad/sound.nix
+    # ../../machines/thinkpad/sound.nix
   ];
 
   services.serial-reader.enable = true;
@@ -42,4 +42,21 @@
   ];
 
   networking = { hostName = "thinkpad"; };
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    #alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
 }
