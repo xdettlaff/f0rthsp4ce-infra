be38b5d6a0132a0efc3dd50bf9e81c951bd26d96
dettlaff <dettlaff@riseup.net>
2024-03-23 20:25:02 +0000

    Update sound.nix

--- machines/thinkpad/sound.nix
+++ machines/thinkpad/sound.nix
@@ -28,4 +28,20 @@
   services.udev.packages =
     [ pkgs.alsa-utils ]; # Добавляем правила udev для ALSA
 
+  services.jack = {
+    jackd.enable = true;
+    # support ALSA only programs via ALSA JACK PCM plugin
+    alsa.enable = false;
+    # support ALSA only programs via loopback device (supports programs like Steam)
+    loopback = {
+      enable = true;
+      # buffering parameters for dmix device to work with ALSA only semi-professional sound programs
+      #dmixConfig = ''
+      #  period_size 2048
+      #'';
+    };
+  };
+
+  users.extraUsers.def.extraGroups = [ "jackaudio" ];
+
 }
